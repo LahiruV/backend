5d9d375c6c019c6ea62b0659e74137b83d981f7e
lahiruvimukthi292 <lahiruvimukthi292@gmail.com>
2024-05-29 09:24:08 +0000

    add

--- routes/questions.js
+++ routes/questions.js
@@ -6,7 +6,6 @@ const router = express.Router();
 // GET: Retrieve all admin users
 router.get('/getmathsQuiz', async (req, res) => {
     const filePath = path.join(__dirname, '../exams/maths.json');
-
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
         console.error('Error reading the file:', err);
@@ -21,6 +20,24 @@ router.get('/getmathsQuiz', async (req, res) => {
         return res.status(500).json({ error: 'Internal server error' });
       }
     });
+});
+
+router.get('/getscienceQuiz', async (req, res) => {
+  const filePath = path.join(__dirname, '../exams/science.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the file:', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    try {
+      const quizData = JSON.parse(data);
+      return res.status(200).json(quizData);
+    } catch (parseError) {
+      console.error('Error parsing JSON data:', parseError);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  });
 });
 
 
