4e254bec9d62215100b029d5366e9674097a3e29
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2024-05-30 23:28:48 +0530

    Question changes

--- routes/admin.js
+++ routes/admin.js
@@ -1,148 +1,155 @@
-const adminSchema = require('../models/admin');
-const bcrypt = require('bcryptjs');
-const express = require('express');
+const adminSchema = require("../models/admin");
+const bcrypt = require("bcryptjs");
+const express = require("express");
 const router = express.Router();
 
 // POST: Register a new admin
-router.post('/register', async (req, res) => {
-    try {
-        const { name, email, phone, password, image } = req.body;
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, phone, password, image } = req.body;
 
-        // Check if the email already exists
-        const existingUser = await adminSchema.findOne({ email });
-        if (existingUser) {
-            return res.status(409).json({ message: 'Email is already used' });
-        }
-
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create new admin
-        const admin = new adminSchema({
-            name,            
-            email,
-            phone,           
-            password: hashedPassword,
-            image
-        });
-
-        // Save the admin
-        await admin.save();
-        res.status(201).json({ message: 'Admin member added successfully' });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    // Check if the email already exists
+    const existingUser = await adminSchema.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email is already used" });
     }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new admin
+    const admin = new adminSchema({
+      name,
+      email,
+      phone,
+      password: hashedPassword,
+      image,
+    });
+
+    // Save the admin
+    await admin.save();
+    res.status(201).json({ message: "Admin member added successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // POST: Admin login
-router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        // Find the user by email
-        const existingUser = await adminSchema.findOne({ email });
-        if (!existingUser) {
-            return res.status(401).json({ message: 'Email or password is incorrect' });
-        }
-
-        // Check the password
-        const passwordMatch = await bcrypt.compare(password, existingUser.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ message: 'Email or password is incorrect' });
-        }
-
-        // Respond success on correct credentials
-        res.status(200).json({ message: 'Login successful', existingUser });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    // Find the user by email
+    const existingUser = await adminSchema.findOne({ email });
+    if (!existingUser) {
+      return res
+        .status(401)
+        .json({ message: "Email or password is incorrect" });
     }
+
+    // Check the password
+    const passwordMatch = await bcrypt.compare(password, existingUser.password);
+    if (!passwordMatch) {
+      return res
+        .status(401)
+        .json({ message: "Email or password is incorrect" });
+    }
+
+    // Respond success on correct credentials
+    res.status(200).json({ message: "Login successful", existingUser });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // GET: Retrieve all admin users
-router.get('/getAll', async (req, res) => {
-    try {
-        // Select all but exclude password field
-        const admins = await adminSchema.find();
-        res.status(200).json(admins);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+router.get("/getAll", async (req, res) => {
+  try {
+    // Select all but exclude password field
+    const admins = await adminSchema.find();
+    res.status(200).json(admins);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // GET: Retrieve an admin by ID
-router.get('/get/:id', async (req, res) => {
-    try {
-        // Extract the ID from the request parameters
-        const { id } = req.params;
+router.get("/get/:id", async (req, res) => {
+  try {
+    // Extract the ID from the request parameters
+    const { id } = req.params;
 
-        // Find the admin by ID in the database
-        const admin = await adminSchema.findById(id);
-        if (!admin) {
-            return res.status(404).json({ message: 'Admin not found' });
-        }
-
-        // Return the found admin
-        res.status(200).json(admin);
-    } catch (err) {
-        if (err.kind === 'ObjectId') {
-            return res.status(400).json({ message: 'Invalid ID format' });
-        }
-        res.status(500).json({ error: err.message });
+    // Find the admin by ID in the database
+    const admin = await adminSchema.findById(id);
+    if (!admin) {
+      return res.status(404).json({ message: "Admin not found" });
     }
+
+    // Return the found admin
+    res.status(200).json(admin);
+  } catch (err) {
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ message: "Invalid ID format" });
+    }
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // DELETE an admin by ID
-router.delete('/delete/:id', async (req, res) => {
-    const { id } = req.params;
+router.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
 
-    try {
-        // Attempt to find and remove the admin by ID
-        const admin = await adminSchema.findByIdAndDelete(id);
+  try {
+    // Attempt to find and remove the admin by ID
+    const admin = await adminSchema.findByIdAndDelete(id);
 
-        // If no admin found, send a 404 response
-        if (!admin) {
-            return res.status(404).json({ message: "Admin not found" });
-        }
-
-        // Send back a success message
-        res.status(200).json({ message: "Admin deleted successfully" });
-    } catch (error) {
-        // Log the error and return a 500 response
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error" });
+    // If no admin found, send a 404 response
+    if (!admin) {
+      return res.status(404).json({ message: "Admin not found" });
     }
+
+    // Send back a success message
+    res.status(200).json({ message: "Admin deleted successfully" });
+  } catch (error) {
+    // Log the error and return a 500 response
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 // PUT: Update an entire admin by ID, with password hashing
-router.put('/update/:id', async (req, res) => {
-    const { id } = req.params;
-    const adminUpdates = req.body;
-  
-    try {
-      // If a new password is provided, hash it
-      if (adminUpdates.password) {
-        const hashedPassword = await bcrypt.hash(adminUpdates.password, 10);
-        adminUpdates.password = hashedPassword;
-      }
-  
-      // Find the admin by ID and update it with the new data
-      // { new: true } option returns the document after update was applied
-      const updatedAdmin = await adminSchema.findByIdAndUpdate(id, adminUpdates, { new: true, runValidators: true });
-  
-      // If no admin found, send a 404 response
-      if (!updatedAdmin) {
-        return res.status(404).json({ message: "Admin not found" });
-      }
-  
-      // Send back the updated admin data, excluding the password field
-      const result = updatedAdmin.toObject();
-      delete result.password;
-      res.status(200).json(result);
-    } catch (error) {
-      // Handle possible errors
-      console.error(error);
-      return res.status(500).json({ error: "Internal server error" });
+router.put("/update/:id", async (req, res) => {
+  const { id } = req.params;
+  const adminUpdates = req.body;
+
+  try {
+    // If a new password is provided, hash it
+    if (adminUpdates.password) {
+      const hashedPassword = await bcrypt.hash(adminUpdates.password, 10);
+      adminUpdates.password = hashedPassword;
     }
-  });
+
+    // Find the admin by ID and update it with the new data
+    // { new: true } option returns the document after update was applied
+    const updatedAdmin = await adminSchema.findByIdAndUpdate(id, adminUpdates, {
+      new: true,
+      runValidators: true,
+    });
+
+    // If no admin found, send a 404 response
+    if (!updatedAdmin) {
+      return res.status(404).json({ message: "Admin not found" });
+    }
+
+    // Send back the updated admin data, excluding the password field
+    const result = updatedAdmin.toObject();
+    delete result.password;
+    res.status(200).json(result);
+  } catch (error) {
+    // Handle possible errors
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = router;
--- routes/questions.js
+++ routes/questions.js
@@ -1,44 +1,199 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const router = express.Router();
+const userSchema = require("../models/user");
+const MathsJson = require("../exams/maths.json");
+const ScienceJson = require("../exams/science.json");
 
 // GET: Retrieve all admin users
-router.get('/getmathsQuiz', async (req, res) => {
-    const filePath = path.join(__dirname, '../exams/maths.json');
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        console.error('Error reading the file:', err);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-  
-      try {
-        const quizData = JSON.parse(data);
-        return res.status(200).json(quizData);
-      } catch (parseError) {
-        console.error('Error parsing JSON data:', parseError);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-    });
-});
-
-router.get('/getscienceQuiz', async (req, res) => {
-  const filePath = path.join(__dirname, '../exams/science.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
+router.get("/getmathsQuiz", async (req, res) => {
+  const filePath = path.join(__dirname, "../exams/maths.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      console.error('Error reading the file:', err);
-      return res.status(500).json({ error: 'Internal server error' });
+      console.error("Error reading the file:", err);
+      return res.status(500).json({ error: "Internal server error" });
     }
 
     try {
       const quizData = JSON.parse(data);
       return res.status(200).json(quizData);
     } catch (parseError) {
-      console.error('Error parsing JSON data:', parseError);
-      return res.status(500).json({ error: 'Internal server error' });
+      console.error("Error parsing JSON data:", parseError);
+      return res.status(500).json({ error: "Internal server error" });
     }
   });
 });
 
+router.get("/getscienceQuiz", async (req, res) => {
+  const filePath = path.join(__dirname, "../exams/science.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading the file:", err);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+
+    try {
+      const quizData = JSON.parse(data);
+      return res.status(200).json(quizData);
+    } catch (parseError) {
+      console.error("Error parsing JSON data:", parseError);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  });
+});
+
+// router.get("/getModuleDetail/:module/:userId", async (req, res) => {
+//   const { module, userId } = req.params;
+//   enum levels {
+//     "Beginner" = 0,
+//     "Intermediate" = 1,
+//     "Advanced" = 2,
+//   }
+
+//   try {
+//     const user = await userSchema.findById(userId);
+
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+
+//     let response;
+//     if (module === "maths") {
+//       let mathsLevel = user.mathsexam.length
+//         ? user.mathsexam[user.mathsexam.length - 1]
+//         : null;
+
+//         response = {
+//           level: mathsLevel?.level ? mathsLevel?.level : "Beginner",
+//           score: mathsLevel?.score ? mathsLevel?.score : 0,
+//           questions: MathsJson[mathsLevel?.level] || MathsJson["Beginner"],
+//         };
+
+//         let currentLevel = levels[mathsLevel?.level];
+//         let nextLevel = currentLevel + 1;
+
+//         if(mathsLevel?.score >= 70){
+//           if(nextLevel <= 2){
+//             response["nextLevel"] = levels[nextLevel];
+//             response["questions"] = MathsJson[levels[nextLevel]];
+//             response["score"] = 0;
+//           } else {
+//             response["nextLevel"] = levels[currentLevel];
+//           }
+//         }
+
+//     } else if (module === "science") {
+//       let scienceLevel = user.scienceexam.length
+//         ? user.scienceexam[user.scienceexam.length - 1]
+//         : null;
+
+//       response = {
+//         level: scienceLevel?.level ? scienceLevel?.level : "Beginner",
+//         questions: ScienceJson[scienceLevel?.level] || ScienceJson["Beginner"],
+//         score: scienceLevel?.score ? scienceLevel?.score : 0,
+//       };
+
+//       let currentLevel = levels[scienceLevel?.level];
+//       let nextLevel = currentLevel + 1;
+
+//       if(scienceLevel?.score >= 70){
+//         if(nextLevel <= 2){
+//           response["nextLevel"] = levels[nextLevel];
+//           response["questions"] = ScienceJson[levels[nextLevel]];
+//           response["score"] = 0;
+//         } else {
+//           response["nextLevel"] = levels[currentLevel];
+//         }
+//       }
+//     }
+
+//     res.status(200).json(response);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
+
+router.get("/getModuleDetail/:module/:userId", async (req, res) => {
+  const { module, userId } = req.params;
+
+  const levels = {
+    Beginner: 0,
+    Intermediate: 1,
+    Advanced: 2,
+  };
+
+  try {
+    const user = await userSchema.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    let response;
+    if (module === "maths") {
+      let mathsLevel = user.mathsexam.length
+        ? user.mathsexam[user.mathsexam.length - 1]
+        : null;
+
+      response = {
+        level: mathsLevel && mathsLevel.level ? mathsLevel.level : "Beginner",
+        score: mathsLevel && mathsLevel.score ? mathsLevel.score : 0,
+        questions:
+          MathsJson[
+            mathsLevel && mathsLevel.level ? mathsLevel.level : "Beginner"
+          ],
+      };
+
+      let currentLevel =
+        levels[mathsLevel && mathsLevel.level ? mathsLevel.level : "Beginner"];
+      let nextLevel = currentLevel + 1;
+
+      if (mathsLevel && mathsLevel.score >= 70) {
+        if (nextLevel <= 2) {
+          response["nextLevel"] = Object.keys(levels)[nextLevel];
+          response["questions"] = MathsJson[Object.keys(levels)[nextLevel]];
+          response["score"] = 0;
+        } else {
+          response["nextLevel"] = Object.keys(levels)[currentLevel];
+        }
+      }
+    } else if (module === "science") {
+      let scienceLevel = user.scienceexam.length
+        ? user.scienceexam[user.scienceexam.length - 1]
+        : null;
+
+      response = {
+        level:
+          scienceLevel && scienceLevel.level ? scienceLevel.level : "Beginner",
+        questions:
+          ScienceJson[
+            scienceLevel && scienceLevel.level ? scienceLevel.level : "Beginner"
+          ],
+        score: scienceLevel && scienceLevel.score ? scienceLevel.score : 0,
+      };
+
+      let currentLevel =
+        levels[
+          scienceLevel && scienceLevel.level ? scienceLevel.level : "Beginner"
+        ];
+      let nextLevel = currentLevel + 1;
+
+      if (scienceLevel && scienceLevel.score >= 70) {
+        if (nextLevel <= 2) {
+          response["nextLevel"] = Object.keys(levels)[nextLevel];
+          response["questions"] = ScienceJson[Object.keys(levels)[nextLevel]];
+          response["score"] = 0;
+        } else {
+          response["nextLevel"] = Object.keys(levels)[currentLevel];
+        }
+      }
+    }
+
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
